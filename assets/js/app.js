545c52a4f6f179a9decc9821f4b9cdcbdb9163b7
blubrd92 <javimt8@gmail.com>
2025-11-06 19:00:36 -0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,7 +29,7 @@
         const generateQrButton = document.getElementById('generate-qr-button');
         const qrCodeCanvas = document.getElementById('qr-code-canvas');
         const qrCodeTextArea = document.getElementById('qr-code-text');
-        const QR_TEXT_PLACEHOLDER = "Enter your text here. Click 'Generate QR' in the settings and test the code with your phone.";
+        const QR_TEXT_PLACEHOLDER = "Enter your blurb here. To link to an online list (like Bibliocommons), paste the URL in the 'Generate QR' field above. Remember to test the code with your phone!";
         
 
         let myBooklist = [];
